4880040a78f2e32541a15710ef183e2571388d94
Miyezo <68035828+m9bmd@users.noreply.github.com>
2024-08-14 11:34:09 +0530

    TextEditor Configured

--- components/blog/TextEditor.tsx
+++ components/blog/TextEditor.tsx
@@ -10,43 +10,30 @@ type TextEditorProps = {
   onChange: (value: string) => void;
 };
 
-const TextEditor = forwardRef(
-  ({ value, onChange, isPending }: TextEditorProps, ref) => {
-    const editor = useEditor({
-      editorProps: {
-        attributes: {
-          class:
-            "min-h-[446px] w-full rounded-md  border border-primary/20  bg-transparent px-3 py-2  ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 overflow-auto ",
-        },
+const TextEditor = ({ value, onChange, isPending }: TextEditorProps) => {
+  const editor = useEditor({
+    editorProps: {
+      attributes: {
+        class:
+          "min-h-[446px] w-full rounded-md  border border-primary/20  bg-transparent px-3 py-2  ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 overflow-auto ",
       },
-      extensions: blogExtensions,
-      content: value,
-      onUpdate: ({ editor }) => {
-        onChange(editor.getHTML());
-      },
-      immediatelyRender: false,
-    });
-    useEffect(() => {
-      if (editor) {
-        editor.setEditable(!isPending);
-      }
-    }, [isPending]);
+    },
+    extensions: blogExtensions,
+    content: value,
+    onUpdate: ({ editor }) => {
+      onChange(editor.getHTML());
+    },
+    immediatelyRender: false,
+  });
 
-    useImperativeHandle(
-      ref,
-      () => ({
-        getEditor: () => editor,
-      }),
-      [editor]
-    );
+  return (
+    <>
+      {editor ? (
+        <TextEditorToolbar editor={editor} isDisabled={isPending} />
+      ) : null}
+      <EditorContent editor={editor} />
+    </>
+  );
+};
 
-    return (
-      <>
-        {editor ? <TextEditorToolbar editor={editor} isDisabled={isPending} /> : null}
-        <EditorContent editor={editor} />
-      </>
-    );
-  }
-);
-TextEditor.displayName = "TextEditor";
 export default TextEditor;
